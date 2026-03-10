d835776fd923a019c3b874c0ea8c1761abedb175
Igor <igorshk271@gmail.com>
2026-03-11 00:32:18 +0200

    feat: add infinity scroll

--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -1,44 +1,57 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {getStories} from "../../API/StoriesService";
 import Loader from "../../UI/Loader/Loader";
 import style from "./stories.module.css";
+import {useObserver} from "../../hooks/useObserver";
 
 function Stories() {
 
     let [stories, setStories] = useState([]);
     let [loading, setLoading] = useState(false);
-    let [totalPages, setTotalPages] = useState(0);
     let [limit, setLimit] = useState(10)
     let [cursor, setCursor] = useState(null);
+    let [page, setPage] = useState(1);
 
+    const lastElement = useRef()
+
+    const fetchStories = async () => {
+        try {
+            setLoading(true);
+            const data = await getStories(limit, cursor);
+            console.log(data)
+            setCursor(data.nextCursor)
+            setStories(prev => [...prev, ...data.stories])
+        } catch (err) {
+            console.log(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        const fetchStories = async () => {
-            try {
-                setLoading(true);
-                const data = await getStories(limit, cursor);
-                setTotalPages(Math.ceil(data.totalCount / 20));
-                setCursor(data.nextCursor)
-                setStories([...stories, ...data.stories])
-            } catch (err) {
-                console.log(err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
-
         fetchStories();
-    }, []);
+    }, [page])
+
+    useObserver(lastElement, cursor !== null && cursor > 1, loading,
+        () => setPage(prev => prev + 1)
+    );
+
+
 
     return (
         <div className={style.stories}>
             {loading && <Loader /> }
             {stories.map((story) => (
                 <div key={story.id}>
-                    <p>{story.id}</p>
-                    <div className={style.story}>{story.title}</div>
+                    <div className={style.story}>
+                        <p>{story.id}</p>
+                        <b>Title : {story.title}</b>
+                        <p>Author : {story.user.username}</p>
+                        <i className='bx bx-right-arrow-alt'></i>
+                    </div>
                 </div>
             ))}
+            <div ref={lastElement} style={{height: 20, background: 'red'}}/>
         </div>
     )
 }
